595a4a527bbee95551d6d28d464566a7acd07982
jackfjo3 <jackfjo3@gmail.com>
2017-03-16 10:41:30 -0400

    count B prolem does not run still

--- ch3hw.js
+++ ch3hw.js
@@ -69,4 +69,30 @@ console.log(isEven(500));
 console.log(isEven(75));
 // → false
 console.log(isEven(-1));
+
+/*
+Bean counting
+
+You can get the Nth character, or letter, from a string by writing "string".charAt(N), similar to how you get its length with "s".length. The returned value will be a string containing only one character (for example, "b"). The first character has position zero, which causes the last one to be found at position string.length - 1. In other words, a two-character string has length 2, and its characters have positions 0 and 1.
+
+Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters are in the string.
+
+Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
+*/
+
+//this code still doesnt work... reports 0 Bs
+
+var countBs = function(textB) {
+  var numberOfBs = 0;
+  for (N=0; N==textB.length - 1; N++;) {
+    if (textB.charAt(N) == 'B') {
+      numberOfBs++;
+    }
+  return numberOfBs;
+  }
+}
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
 // → ??
