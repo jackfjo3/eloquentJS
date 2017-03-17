04ad9b7f8478429df3cd5d75c7013c73d4b1181f
jackfjo3 <jackfjo3@gmail.com>
2017-03-17 16:55:15 -0400

    reverse in place still not functional

--- ch4hw.js
+++ ch4hw.js
@@ -41,3 +41,64 @@ console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
 console.log(sum(range(1, 10)));
 // → 55
+
+
+/*
+Reversing an array
+
+Arrays have a method reverse, which changes the array by inverting the order in which its elements appear. For this exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order. The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument in order to reverse its elements. Neither may use the standard reverse method.
+
+Thinking back to the notes about side effects and pure functions in the previous chapter, which variant do you expect to be useful in more situations? Which one is more efficient?
+
+*/
+
+/* FIRST ATTEMPT
+function reverseArray(origArray) {
+  var newArray = [];
+  // I don't like iterating backwards, will redo with upshift method instead
+  for (i = origArray.length-1; i >= 0; i--) {
+    newArray.push(origArray[i]);
+  }
+  return newArray;
+}
+*/
+
+
+
+
+// Your code here.
+
+function reverseArray(origArray) {
+  var newArray = [];
+ 
+  for (i = 0; i <= origArray.length-1; i++) {
+    newArray.unshift(origArray[i]);
+  }
+  return newArray;
+}
+
+// reversing the arguments 'in place' rather than simply creating a new array
+
+//THIS DOESN'T WORK YET... NOT TOO COMFORTABLE WITH WORKING DIRECTLY WITH ARGUMENT MANIPULATION YET.
+
+function reverseArrayInPlace(swapArgs) {
+
+  for (i = 0; i < Math.floor(swapArgs.length / 2); i++) { //only need to iterate through half of arguments because that's when you start to run into the mirrored positions in the 'array' that you've already swapped. Round down (floor) so that middle value of an odd-numbered array of arguments is left unchanged
+    var movingArg = swapArgs[i];
+    swapArgs[i] = swapArgs[swapArgs.length - i - 1];
+    swapArgs[swapArgs.length - i - 1] = movingArg;
+  
+  }
+  
+  return swapArgs;
+  
+}
+
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+
+var arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
