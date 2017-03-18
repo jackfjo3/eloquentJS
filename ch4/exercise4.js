d69bce698c1e0f29ff6a8911cbf84684e20f0a21
jackfjo3 <jackfjo3@gmail.com>
2017-03-18 18:10:37 -0400

    deepEqual funciton works and correctly identifies if an object has the same values

--- ch4/exercise4.js
+++ ch4/exercise4.js
@@ -4,12 +4,26 @@ function deepEqual(thing1, thing2) {
     return true;
   }
   // otherwise, check if both things are objects (and also not null which is technically an object type)
-  else if (typeof thing1 === 'object' && typeof thing1 !== null && typeof thing1 === typeof thing1) {
-    // CHECK FOR EQUAL PROPERTIES
-    return 'objects that are not null'
+  else if (typeof thing1 === 'object' && typeof thing2 === typeof thing1 && typeof thing1 !== null ) {
+    // check thing1 for a property and name if nextProperty
+    for (var nextProperty in thing1) {
+      // if thing 2 has the same property, check the deep-equality of the properties.
+      if (nextProperty in thing2) {
+        return deepEqual(thing1[nextProperty], thing2[nextProperty]);
+      }
+    }
   }
-  
+  // if something is not the same, they are not deeply equal
   else {
-    return 'not deeply equal';
+    return false;
   }
 }
+
+
+var obj = {here: {is: "an"}, object: 2};
+console.log(deepEqual(obj, obj));
+// → true
+console.log(deepEqual(obj, {here: 1, object: 2}));
+// → false
+console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));
+// → true
