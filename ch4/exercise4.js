ea58fa452a9887aa3316d281f4a1173dead7b331
jackfjo3 <jackfjo3@gmail.com>
2017-03-18 18:14:28 -0400

    deepEqual concerns added to comments

--- ch4/exercise4.js
+++ ch4/exercise4.js
@@ -1,3 +1,5 @@
+// one lingering concern is whether this will catch the difference between thing1 and thing2 if thing2 has all the same name:value pairs as thing1 PLUS additional name:value elements. You could have this funciton rerun itself with thing1 and thing2 swapped in the argument positions to double check this. There are likely ways to further reduce redundancies in this double-check strategy 
+
 function deepEqual(thing1, thing2) {
   // if the two things are equal values, than they are the simplest example of deeply equal
   if (thing1 === thing2) {
